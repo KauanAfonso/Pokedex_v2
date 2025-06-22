c2683b2e8cb42edbb5db0821f178954264131b13
KauanAfonso <kauanafon3@gmail.com>
2025-06-22 11:31:30 -0300

    fazendo o card e conhecendo a api

--- Poke/pages/Index.jsx
+++ Poke/pages/Index.jsx
@@ -1,21 +1,26 @@
 import axios from 'axios';
 import { useEffect } from 'react';
+import { Card } from '../src/componentes/Card';
+import styles from './Index.module.css';
 
 export function Index() {
     useEffect(() => {
         async function fetchPokemons() {
             try {
                 const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=10');
-                const pokemons = response.data.results;
-                console.log(pokemons);
+                const pokemons_base = response.data.results;
+                console.log(pokemons_base);
 
-                for (const pokemon of pokemons) {
+                for (const pokemon of pokemons_base) {
                     const pokemonResponse = await axios.get(pokemon.url);
+                    //pegando a url de species
+                    const species = await axios.get(pokemonResponse.data.species.url);
+                    console.log(species.data);
                     console.log(pokemonResponse.data);
                 }
 
             } catch (error) {
-                console.error('Error fetching Pokemons:', error);
+                console.error('Error fetching Pokemons_base:', error);
             }        
         }
 
@@ -23,6 +28,10 @@ export function Index() {
     }, []); 
   
     return(
-        <h1>Hello world , Kauan</h1>
+        <main>
+            <div className={styles.container_main}>
+
+            </div>
+        </main>
     )
 }
---
+++ Poke/src/componentes/Card.jsx
@@ -0,0 +1,13 @@
+import styles from "./Card.module.css";
+
+export function Card({ name, img, type, height, weight, color = "white" }) {
+  return (
+    <div className={styles.Card} style={{ backgroundColor: color }}>
+      <h2>{name}</h2>
+      <img src={img} alt="Pokemon" />
+      <p>Type: {type}</p>
+      <p>Height: {height}</p>
+      <p>Weight: {weight}</p>
+    </div>
+  );
+}
